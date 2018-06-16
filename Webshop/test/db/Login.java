df0b847ba12407f6903ae376243ebdc5b7440863
Carolyne Peelman <peelmancarolyne@gmail.com>
2018-06-16 13:32:30 +0200

    Removed demos and old version project from this repo.

--- Webshop/test/db/Login.java
+++
@@ -1,7 +0,0 @@
-package db;
-
-public class Login {
-	protected static String user = "r0458882";
-	protected static String password = "0493795484Cp?";
-
-}
--- Webshop/test/ui/Login.java
+++
@@ -1,7 +0,0 @@
-package ui;
-
-public class Login {
-	protected static String user = "r0458882";
-	protected static String password = "0493795484Cp?";
-
-}
